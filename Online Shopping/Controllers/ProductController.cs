c69f07b80eccbe9e0e9c5ce82ff0d8fa44a4dcd0
manish kumar <mr.manish4uall@outlook.com>
2018-11-16 22:22:57 +0530

    Added a new model

--- Online Shopping/Controllers/ProductController.cs
+++ Online Shopping/Controllers/ProductController.cs
@@ -8,10 +8,42 @@ namespace Online_Shopping.Controllers
 {
     public class ProductController : Controller
     {
+        {
         // GET: Product
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult CreateProduct()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(ProductModel obj)
+        {
+            using (ShoppingDBEntities objDBshopping = new ShoppingDBEntities())
+            {
+                try
+                {
+                    tblProduct objTblprod = new tblProduct();
+
+                    objTblprod.ProductName = obj.ProductName;
+                    objTblprod.Description = obj.ProductDesc;
+                    objTblprod.UnitPrice = Convert.ToInt32(obj.ProductUnitPrice);
+                    objTblprod.Unit = obj.ProductUnit;
+                    objTblprod.Category = obj.CategoryId;
+                    objTblprod.isActive = true;
+
+                    objDBshopping.tblProducts.Add(objTblprod);
+                    objDBshopping.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+
+            }
+            TempData["regStatus"] = "Registration Successful";
+            return RedirectToAction("ShowProduct");
         }
     }
 }
---
+++ Online Shopping/Models/ProductModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shopping.Models
+{
+    public class ProductModel
+    {
+        [Required(ErrorMessage = "Please Enter Product Name")]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Description")]
+        public string ProductDesc { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Unit Price")]
+        [Display(Name = "Unit Price")]
+        public string ProductUnitPrice { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Unit")]
+        [Display(Name = "Unit")]
+        public string ProductUnit { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Category Name")]
+        [Display(Name = "Category")]
+        public string CategoryId { get; set; }
+    }
+}
